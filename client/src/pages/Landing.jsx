4a84b50d8292e30966598111f8201f4972cd546a
ayaanqasmi <ayaan.qasmi@gmail.com>
2024-12-15 14:43:52 +0500

    login and landing

---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,36 @@
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import Categories from "../components/Categories";
+import Bestseller from "../components/Bestseller";
+import FeaturedArtisan from "../components/FeaturedArtisan";
+import Socials from "../components/Socials";
+import Testimonial from "../components/Testimonial";
+import Footer from "../components/Footer";
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
+const Landing = () => {
+  const {user} = useContext(UserContext)
+  return (
+    <div>
+      <Navbar
+        links={[
+          {button: false, path: "#top", name: "Top Picks"},
+          {button: false, path: "#artisan", name: "Featured Artisans"},
+          {button: false, path: "#testimonial", name: "Testimonials"},
+          { button: true, path: "/login", btn_name: "Login" },
+          { button: true, path: "/register", btn_name: "Register" },
+
+        ]}
+      />
+      <Hero/>
+      <Categories />
+      <Bestseller user={user} />
+      <FeaturedArtisan />
+      <Socials />
+      <Testimonial />
+      <Footer/>
+    </div>
+  );
+};
+document.body.removeAttribute("class");
+export default Landing;
