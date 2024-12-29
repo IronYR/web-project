7dd6041f7d04a9f55eb72ace53b708b310900439
Yousuf <yrehan67@gmail.com>
2024-12-30 00:40:03 +0500

    cors

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ const {mongoose} = require ('mongoose')
 const stripe = require ("./routes/stripe")
 //middleware
 const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
+    origin: '*', // Allow access from anywhere
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200
 }
 app.use(cors(corsOptions))
 app.use(express.json())
