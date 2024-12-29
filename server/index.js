2c5b067cac3627d6848823059d6e803f1e329206
Yousuf <yrehan67@gmail.com>
2024-12-30 00:34:42 +0500

    fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const stripe = require ("./routes/stripe")
 //middleware
 const corsOptions ={
     origin:'http://localhost:3000', 
-    credentials:false,            //access-control-allow-credentials:true
+    credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
 app.use(cors(corsOptions))
